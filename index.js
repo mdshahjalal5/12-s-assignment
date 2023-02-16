1271b35bcb55af118f12a4d742128f018809eafe
mdshahjalal5 <muhommodshahjalal@gmail.com>
2023-02-16 15:14:49 +0600

    category xiomi data found

--- index.js
+++ index.js
@@ -4,44 +4,26 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
-
 // middleware
 app.use(cors());
 app.use(express.json());
-
-
-const uri = `mongodb+srv://${process.env.nameUser}:${process.env.password}@cluster0.ckb7hbl.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.ckb7hbl.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-// console.log('uri', uri)
+const productCollection = client.db('12-assignment').collection('allServices')
+
 async function run() {
     try {
-        const productCollection = client.db('emaJohn').collection('products')
-        // app.get('/products', async(req, res)=>{
-        //         const response =  await productCollection.find({}).toArray();
-        //         console.log(response, 'respnse');
-        //         res.send(response)
-        //     })
         app.get('/products', async (req, res) => {
-            const page = parseInt(req.query.page);
-            const size = parseInt(req.query.size);
-            console.log(page, size, 'page, size');
-            const query = {}
-            const cursor = productCollection.find(query);
-            const products = await cursor.skip(page * size).limit(size).toArray();
-            const count = await productCollection.estimatedDocumentCount();
-            console.log(count, 'count get');
-            res.send({ count, products });
-        });
-
-        app.post('/productsByIds', async (req, res) => {
-            const ids = req.body;
-            const objectIds = ids.map(id => new ObjectId(id))
-            const query = { _id: { $in: objectIds } };
-            const cursor = productCollection.find(query);
-            const products = await cursor.toArray();
-            res.send(products);
+            let query = {};
+            if (req.query.category){
+                const category = req.query.category;
+                console.log(category, 'category2');
+                query = { category}
+            }
+            const response = await productCollection.find(query).toArray();
+            console.log(response, 'respnse');
+            res.send(response)
         })
-
     }
     finally {
 
@@ -50,9 +32,9 @@ async function run() {
 run().catch(err => console.error(err, 'error'));
 
 app.get('/', (req, res) => {
-    res.send('ema john server is running');
+    res.send('12 assignment server is running');
 })
 
 app.listen(port, () => {
-    console.log(`ema john running on: ${port}`)
+    console.log(`12 assignment running on: ${port}`)
 })
